4551f3f2197a43b2f51b32dde5da2d048a4efb14
JacobR77 <42145046+JacobR77@users.noreply.github.com>
2018-08-06 16:14:01 +0100

    Add files via upload

---
+++ practicepython_exercise3_listlessthan10.py
@@ -0,0 +1,10 @@
+#write a program that prints out all elements of a list less than 5
+
+a = [1,1,2,3,5,8,13,21,24,55,89]
+
+def b():
+	for element in a:
+		if element < 5:
+			print(element)
+
+b()			
